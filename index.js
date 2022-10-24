fe09257f8b761691ff698923edc0b829836ba3c1
Mark Sauer <39628206+sauerm1@users.noreply.github.com>
2022-10-24 17:05:47 -0400

    add https support

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const port = 3000;
 
-const app = express();
+var key = fs.readFileSync(__dirname + '/../certs/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/../certs/selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
 
-const PORT = process.env.PORT || 3000;
+app = express()
+app.get('/', (req, res) => {
+   res.send('Now using https..');
+});
 
-app.get('/',(req, res) => res.send('Hello World'));
+var server = https.createServer(options, app);
 
-app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
+server.listen(port, () => {
+  console.log("server starting on port : " + port)
+});
+
+// const express = require('express');
+
+// const app = express();
+
+// const PORT = process.env.PORT || 3000;
+
+// app.get('/',(req, res) => res.send('Hello World'));
+
+// app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
