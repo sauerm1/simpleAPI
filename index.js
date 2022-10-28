fe6c38645148b721b6aa4715ee39ac4349f2e609
Mark Sauer <39628206+sauerm1@users.noreply.github.com>
2022-10-28 16:04:42 -0400

    update to HTTPS

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('../certs/client-key.pem'),
+  cert: fs.readFileSync('../certs/client-cert.pem')
+};
+
+// Create a service (the app object is just a callback).
+var app = express();
+app.get('/',(req, res) => {
+    console.log(req.rawHeaders)
+    res.send('Hello World')
+});
+
+
+// Create an HTTP service.
+http.createServer(app).listen(80);
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(443);
+
 // const express = require('express');
-// const https = require('https');
-// const fs = require('fs');
-// const port = 3000;
 
-// var key = fs.readFileSync(__dirname + '/../certs/selfsigned.key');
-// var cert = fs.readFileSync(__dirname + '/../certs/selfsigned.crt');
-// var options = {
-//   key: key,
-//   cert: cert
-// };
+// const app = express();
 
-// app = express()
-// app.get('/', (req, res) => {
-//    res.send('Now using https..');
-// });
+// const PORT = process.env.PORT || 3000;
 
-// var server = https.createServer(options, app);
+// app.get('/',(req, res) => res.send('Hello World'));
 
-// server.listen(port, () => {
-//   console.log("server starting on port : " + port)
-// });
-
-const express = require('express');
-
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.get('/',(req, res) => res.send('Hello World'));
-
-app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
+// app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
